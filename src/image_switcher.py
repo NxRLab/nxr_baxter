7b1c9df4337ecc84cb6593341bed33b7483c72b8
Adam Barber <adb421@gmail.com>
2014-03-04 13:46:48 -0600

    Mode switching and updating and period changing work, just need to actually implement publishing to head and then test on Baxter.

--- src/image_switcher.py
+++ src/image_switcher.py
@@ -28,32 +28,43 @@ class ImageSwitcher(object):
     def _imageUpdate(self, reset=False):
         """
         Does the majority of the work for this class. Will display the next
-        image in the sequence, or restart the sequence if it hasn't been started
+        image in the sequence, or restart the sequence if it hasn't been
+        started. Should  only be called by the timer callback or whenever the
+        mode/period has been changed and the timer needs to be reset.
         """
-        print "something"
+        if reset:
+            print "Resetting, mode: %s" % self._mode
+        else:
+            print "Continuing mode: %s" % self._mode
     
     # written first without file stuff, lets just get it working hardcoded
     def __init__(self, mode='idle', image_period=3.0):
+        """
+        Constructor for the class, optionally takes a mode and an image
+        switching period. Then starts the timer.
+        """
         self._mode = mode
         self.image_timer = None
         self._image_period = image_period
-        self.startTimer()
-        print "Got here"
+        self._startTimer()
 
-    def change_mode(self, newMode=self._mode, newPeriod=self._image_period):
+    def change_mode(self, newMode=None, newPeriod=None):
         """
         Takes in a new mode type and/or a new image switching period. Note that
         even if neither of these change, the timer will be reset.
         """
-        self._mode = newMode
-        self._image_period = newPeriod
-        self.startTimer()
+        if newMode:
+            self._mode = newMode
+        if newPeriod:
+            self._image_period = newPeriod
+        self._startTimer()
 
-    def change_period(self, newPeriod):
-        self._image_period = newPeriod
-        self.startTimer()
-
-    def startTimer(self):
+    def _startTimer(self):
+        """
+        This function shuts down the timer if it has been running, prints the
+        first image in the sequence (by resetting _imageUpdate()) and then
+        starts the timer with the stored period.
+        """
         if self.image_timer:
             self.image_timer.shutdown()
         # Note that the timer won't show an image until the timer goes off, so 
@@ -69,6 +80,6 @@ if __name__=="__main__":
     img_sw = ImageSwitcher()
     print "made object"
     rospy.sleep(10)
-    img_sw.change_mode('mode 2')
+    img_sw.change_mode('mode 2',1)
     rospy.spin()
     
