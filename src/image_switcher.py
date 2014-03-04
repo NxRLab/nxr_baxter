9625b6d67d770df5ccf384b3c95678e2683fd239
Adam Barber <adb421@gmail.com>
2014-03-04 13:30:07 -0600

    Increasing functionality of the ImageSwitcher class by setting it up so that mode and period can be switched simultaneously with a reset option for the image updater method.

--- src/image_switcher.py
+++ src/image_switcher.py
@@ -17,27 +17,49 @@ class ImageSwitcher(object):
 
     """
     def timerCallback(self, event):
-        print "Mode: ", self.mode
+        """
+        Callback function for the image switching period timer. Calls the image
+        update function with the default 'False' value for the reset. This means
+        that anytime the function gets called out of this method, it will not
+        assume  the mode has been changed.
+        """
+        self._imageUpdate()
+
+    def _imageUpdate(self, reset=False):
+        """
+        Does the majority of the work for this class. Will display the next
+        image in the sequence, or restart the sequence if it hasn't been started
+        """
+        print "something"
     
     # written first without file stuff, lets just get it working hardcoded
     def __init__(self, mode='idle', image_period=3.0):
-        self.mode = mode
-        self.modeChanged = True
+        self._mode = mode
         self.image_timer = None
-        self.image_period = image_period
+        self._image_period = image_period
         self.startTimer()
         print "Got here"
 
-    def change_mode(self, newMode):
-        self.mode = newMode
-        self.modeChanged = True
-        #re-run the timer callback function now
+    def change_mode(self, newMode=self._mode, newPeriod=self._image_period):
+        """
+        Takes in a new mode type and/or a new image switching period. Note that
+        even if neither of these change, the timer will be reset.
+        """
+        self._mode = newMode
+        self._image_period = newPeriod
+        self.startTimer()
+
+    def change_period(self, newPeriod):
+        self._image_period = newPeriod
         self.startTimer()
 
     def startTimer(self):
         if self.image_timer:
             self.image_timer.shutdown()
-        self.image_timer = rospy.Timer(rospy.Duration(self.image_period),self.timerCallback)
+        # Note that the timer won't show an image until the timer goes off, so 
+        # let's call the function that the callback calls once
+        self._imageUpdate(reset=True)
+        self.image_timer = rospy.Timer(rospy.Duration(self._image_period),self.timerCallback)
 
 
 
