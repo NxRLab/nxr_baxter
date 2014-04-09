e681616b09778550c6262345bcf46848d37c5cd3
Adam Barber <adb421@gmail.com>
2014-04-09 17:11:57 -0500

    Testing done for test_plan_speeds, clearly joint limits affect the trajectory times. We need to check if they are up to date.

--- src/test_plan_speeds.py
+++ src/test_plan_speeds.py
@@ -15,23 +15,29 @@ import moveit_msgs.msg
 
 import trajectory_msgs.msg
 
+import subprocess
+
 
 if __name__=='__main__':
+    cmd = 'rosrun baxter_interface joint_trajectory_action_server.py'
+    subprocess.Popen(cmd,shell=True)
     rospy.init_node('planning_test', log_level=rospy.INFO)
     rospy.loginfo("Testing plan speeds for moveit")
     
-    self.mime_l_angles = {'left_s0': 0.35, 'left_s1': 0.00, 'left_e0': 0.00, 'left_e1': 0.00, 'left_w0': 0.00, 'left_w1': 0.00, 'left_w2': 0.00}
-    self.mime_r_angles = {'right_s0': -0.25, 'right_s1': 0.00, 'right_e0': 0.00, 'right_e1': 0.00, 'right_w0': 0.00, 'right_w1': 0.00, 'right_w2': 0.00}
+    mime_l_angles = {'left_s0': 0.35, 'left_s1': 0.00, 'left_e0': 0.00, 'left_e1': 0.00, 'left_w0': 0.00, 'left_w1': 0.00, 'left_w2': 0.00}
+    mime_r_angles = {'right_s0': -0.25, 'right_s1': 0.00, 'right_e0': 0.00, 'right_e1': 0.00, 'right_w0': 0.00, 'right_w1': 0.00, 'right_w2': 0.00}
+    
+    rospy.sleep(5)
 
     robot = moveit_commander.RobotCommander()
     scene = moveit_commander.PlanningSceneInterface()
 
     both_arms_group = moveit_commander.MoveGroupCommander("both_arms")
 
-    rospy.loginfo("Get planning time: %d", both_arms_group.get_planning_time())
+    rospy.loginfo("Get planning time: %f", both_arms_group.get_planning_time())
 
-    both_arms_group.set_joint_value_target(moveit_both_arms_group.set_joint_value_target(dict(crane_r_angles, **crane_l_angles)))
+    both_arms_group.set_joint_value_target(dict(mime_r_angles, **mime_l_angles))
 
     traj = both_arms_group.plan()
     lastTime = traj.joint_trajectory.points[len(traj.joint_trajectory.points)-1].time_from_start
-    rospy.loginfo("Trajectory time is: %d", lastTime)
+    rospy.loginfo("Trajectory time is: %f", lastTime)
