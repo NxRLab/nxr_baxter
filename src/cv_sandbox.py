4ad69388ed2a069cd15c13e7ad18573f9ba9a064
Jon <jonathanrovira2016@u.northwestern.edu>
2014-04-29 04:29:31 -0500

    cv_sandbox lookin good

--- src/cv_sandbox.py
+++ src/cv_sandbox.py
@@ -14,43 +14,66 @@ from sensor_msgs.msg import Image
 import baxter_interface
 
 
+class vision(object):
 
-def start_camera():
-	# Instantiate all three cameras
-	self._left_camera = baxter_interface.CameraController('left_hand_camera')
-	self._right_camera = baxter_interface.CameraController('right_hand_camera')
-	self._head_camera = baxter_interface.CameraController('head_camera')
+	def __init__(self):
+		# Instantiate all three cameras
+		self._left_camera = baxter_interface.CameraController('left_hand_camera')
+		self._right_camera = baxter_interface.CameraController('right_hand_camera')
+		self._head_camera = baxter_interface.CameraController('head_camera')
 
-	# Close all three cameras
-	self._left_camera.close()
-	self._right_camera.close()
-	self._head_camera.close()
+		# Head screen publisher
+		self.image_pub = rospy.Publisher('/robot/xdisplay', Image, latch=True)
+		self._bridge = CvBridge()
+		cv.NamedWindow("Image window", 1)
 
-	# Open left camera
-	self._left_camera.open()
+		# Start left hand camera
+		self.start_camera()
 
-	# Subscribe to left camera's feed
-	camera_topic = '/cameras/left_hand_camera/image'
-    _camera_sub = rospy.Subscriber(
-        camera_topic,
-        Image,
-        self._on_camera) # Runs as callback
+	def put_image_on_screen(self):
+		img = cv.LoadImage('/home/nxr-baxter/groovyws/src/nxr_baxter/images/Display-Booleans-Reset.jpg')
+		msg = CvBridge().cv_to_imgmsg(img, encoding="bgr8")
+		pub = rospy.Publisher('/robot/xdisplay', Image, latch=True)
+		pub.publish(msg)
+		rospy.sleep(1)
 
-def _on_camera(self, data):
-	try:
-		self.cv_image = self._bridge.imgmsg_to_cv(data, "bgr8")
-		local_image = np.asarray(self.cv_image)
+	def start_camera(self):
+		# Close all three cameras
+		self._left_camera.close()
+		self._right_camera.close()
+		self._head_camera.close()
 
-		img = self.cv_image
-	    msg = cv_bridge.CvBridge().cv_to_imgmsg(img, encoding="bgr8")
-	    pub = rospy.Publisher('/robot/xdisplay', Image, latch=True)
-	    pub.publish(msg)
-	except Exception:
-		print 'OH NO - IMAGE WENT WRONG!!'
+		# Open left camera
+		self._left_camera.open()
+		self._left_camera.resolution = [1280, 800]
+
+		# Subscribe to left camera's feed
+		camera_topic = '/cameras/left_hand_camera/image'
+		_camera_sub = rospy.Subscriber(
+	        camera_topic,
+	        Image,
+	        self._on_camera) # Runs as callback
+
+	def _on_camera(self, data):
+		try:
+			cv_image = self._bridge.imgmsg_to_cv(data, "bgr8")
+
+		except CvBridgeError, e:
+			print e
+
+		cv.ShowImage("Image window", cv_image)
+		msg = self._bridge.cv_to_imgmsg(cv_image, encoding="bgr8")
+		self.image_pub.publish(msg)
 
 
 def main():
-	start_camera()
+	rospy.init_node('cv_sandbox')
+	v = vision()
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print "Shutting down"
+	cv.DestroyAllWindows()
 
 
 
