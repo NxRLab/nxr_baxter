a92573aacd9fa30ef79b669819d72634e27f8daf
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-06-17 22:07:14 -0500

    dumbass

--- src/june20thDemo.py
+++ src/june20thDemo.py
@@ -81,17 +81,17 @@ class Dancer(object):
         num_positions = 3
         right_times = [1, 2, 3, 4]
         left_times = [1, 2, 3, 4]
-        right_positions = [[ 0.60,  0.00, 0.00, 0.00, 2.75, 0.00, 0.00], 
-                          [ 0.60,  0.40, 2.90, 2.10, 0.00, 1.20, 0.00],
-                          [-0.60, -0.20, 0.00, 2.00, 0.00, 0.50, 0.00]]
-        left_positions = [[ -0.60,  0.00,  0.00, 0.00, -2.75, 0.00, 0.00], 
-                          [ -0.60,  0.40, -2.90, 2.10,  0.00, 1.20, 0.00],
-                          [  0.60, -0.20,  0.00, 2.00,  0.00, 0.50, 0.00]]
+        right_positions = [[ 0.60,  0.00,  0.00,  0.00,  2.75,  0.00,  0.00], 
+                          [  0.60,  0.40,  2.90,  2.10,  0.00,  1.20,  0.00],
+                          [ -0.60, -0.20,  0.00,  2.00,  0.00,  0.50,  0.00]]
+        left_positions = [[ -0.60,  0.00,  0.00,  0.00, -2.75,  0.00,  0.00], 
+                          [ -0.60,  0.40, -2.90,  2.10,  0.00,  1.20,  0.00],
+                          [  0.60, -0.20,  0.00,  2.00,  0.00,  0.50,  0.00]]
 
         print("Dancing. Press Ctrl-c to stop...")
         for i in range(num_positions):
             while not rospy.is_shutdown():
-                right_angles = dict(zip(self._right_joint_names, left_positions[i]))
+                right_angles = dict(zip(self._right_joint_names, right_positions[i]))
                 left_angles = dict(zip(self._left_joint_names, left_positions[i]))
 
                 self._right_arm.move_to_joint_positions(right_angles)
