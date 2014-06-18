7113ee296371320391e08d463d90491742e8a958
Adam Barber <adb421@gmail.com>
2014-06-18 15:34:18 -0500

    IK not working. Results are not valid. Very strange. Could this be the problem with moveit also

--- src/test_moveit.py
+++ src/test_moveit.py
@@ -9,15 +9,19 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 
+from std_msgs.msg import Header
+
 from baxter_core_msgs.srv import (
-    SolvePositionIK
+    SolvePositionIK,
     SolvePositionIKRequest)
 
 from geometry_msgs.msg import (
-    PoseStamped
-    Pose)
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion)
 
-from tf.transformations import createQuaternionFromRPY
+from tf.transformations import quaternion_from_euler
 
 if __name__=='__main__':
     rospy.loginfo("Starting test_moveit node")
@@ -25,30 +29,38 @@ if __name__=='__main__':
 
     right_arm_group = moveit_commander.MoveGroupCommander("right_arm")
 
-    pose = [0.28, -0.62, -0.32, 3.14/4, -3.14/2, 0]
-    # pose = [0.815, -1.01, 0.321, 0.271, 0.653, -0.271, 0.653]
+    # des_pose = [0.28, -0.62, -0.32, 0, -3.14/2, 0]
+    des_pose = [0.815, -1.01, 0.321, 0.271, 0.653, -0.271, 0.653]
 
-    limb = "left_right"
+    limb = "left"
     ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
     iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
     ikreq = SolvePositionIKRequest()
     hdr = Header(stamp=rospy.Time.now(), frame_id='base')
-    posestamp = Pose(
-        position=Point(pose[0:3]),
-        orienation=createQuaternionFromRPY(pose[3:])
-    )
-    ikreq.pose_stamp.append(PoseStamped(header=hdr, pose=pose))
+    # quat = quaternion_from_euler(des_pose[3],des_pose[4],des_pose[5])
+    pose = Pose()
+    quat = Quaternion()
+    quat.x = des_pose[3]
+    quat.y = des_pose[4]
+    quat.z = des_pose[5]
+    quat.w = des_pose[6]
+    pose.orientation = quat
+    pose.position.x = des_pose[0]
+    pose.position.y = des_pose[1]
+    pose.position.z = des_pose[2]
+    ikreq.pose_stamp = [PoseStamped(header=hdr, pose=pose)]
+    # print ikreq
     try:
         rospy.wait_for_service(ns, 5.0)
         resp = iksvc(ikreq)
     except (rospy.ServiceException, rospy.ROSException), e:
         rospy.loginfo("Service exception")
 
-    if resp.result_type[0] != resp.RESULT_INVALID:
+    if resp.isValid[0]:
         des_joints = resp.joints[0];
 
-        print right_arm_group.get_current_pose()
-
+        # print right_arm_group.get_current_pose()
+        # print des_joints
         right_arm_group.set_joint_value_target(des_joints)
 
         right_arm_group.plan()
@@ -57,4 +69,5 @@ if __name__=='__main__':
         print right_arm_group.get_current_pose()
 
     else:
+        print right_arm_group.get_current_pose()
         print "IK results were not valid."
