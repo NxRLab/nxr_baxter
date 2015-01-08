f2d7a2abba8e8e7304df6345f30b6bc68fb6fa3a
Jon <jonathanrovira2016@u.northwestern.edu>
2015-01-08 12:40:39 -0600

    Cleaning up comments.

--- src/test_moveit.py
+++ src/test_moveit.py
@@ -107,43 +107,4 @@ if __name__=='__main__':
 
     move_pos(des_pose2)
 
-    # limb = "right"
-    # ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
-    # iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
-    # ikreq = SolvePositionIKRequest()
-    # hdr = Header(stamp=rospy.Time.now(), frame_id='base')
-    # # quat = quaternion_from_euler(des_pose[3],des_pose[4],des_pose[5])
-    # pose = Pose()
-    # quat = Quaternion()
-    # quat.x = des_pose[3]
-    # quat.y = des_pose[4]
-    # quat.z = des_pose[5]
-    # quat.w = des_pose[6]
-    # pose.orientation = quat
-    # pose.position.x = des_pose[0]
-    # pose.position.y = des_pose[1]
-    # pose.position.z = des_pose[2]
-    # ikreq.pose_stamp = [PoseStamped(header=hdr, pose=pose)]
-    # # print ikreq
-    # try:
-    #     rospy.wait_for_service(ns, 5.0)
-    #     resp = iksvc(ikreq)
-    # except (rospy.ServiceException, rospy.ROSException), e:
-    #     rospy.loginfo("Service exception")
-
-    # if resp.isValid[0]:
-    #     des_joints = [0]*7
-    #     for i in range(7):
-    #         des_joints[i] = resp.joints[0].position[i]
-    #     # print right_arm_group.get_current_pose()
-    #     # print des_joints
-    #     right_arm_group.set_joint_value_target(des_joints)
-
-    #     right_arm_group.plan()
-    #     right_arm_group.go()
     
-    #     print right_arm_group.get_current_pose()
-
-    # else:
-    #     print right_arm_group.get_current_pose()
-    #     print "IK results were not valid."
